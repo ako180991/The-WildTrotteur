5102f61a5417c14f8ec21f20e6bbc6e41a63c679
Florian <florianplot@msn.com>
2019-09-27 14:01:46 +0200

    Ajout des js pour home et voyage

---
+++ home.js
@@ -0,0 +1,27 @@
+let change = document.getElementById('sesame')
+let change1 = document.getElementById('sesame1')
+let change2 = document.getElementById('sesame2')
+
+function trigger () {
+    if (change.style.display == 'block' ) {
+        change.style.display = 'none'
+    } else {
+        change.style.display = 'block'
+    }
+}
+
+function trigger1 () {
+    if (change1.style.display == 'block' ) {
+        change1.style.display = 'none'
+    } else {
+        change1.style.display = 'block'
+    }
+}
+
+function trigger2 () {
+    if (change2.style.display == 'block' ) {
+        change2.style.display = 'none'
+    } else {
+        change2.style.display = 'block'
+    }
+}
